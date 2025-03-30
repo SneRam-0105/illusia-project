2ce04f13dde53e76baab88503c0b534420f68126
Sneha <133229057+SneRam-0105@users.noreply.github.com>
2025-03-30 12:50:17 +0300

    updated docker compose file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
+import { Box, Typography } from "@mui/material";
 
-
-function App() {
+const App = () => {
   return (
-    <>
-    Home page.
-    </>
-  )
-}
-
-export default App
+    <Box sx={{ margin: "auto", textAlign: "center", maxWidth: 600, px: 2 }}>
+      <Box sx={{ paddingTop: 10, mb: 4 }}>
+        <Typography variant="h3">Welcome!</Typography>
+        <Typography variant="subtitle1">Log in to your account.</Typography>
+      </Box>
+      ;
+    </Box>
+  );
+};
+export default App;
